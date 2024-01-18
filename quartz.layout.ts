883b952256354c98e883ba6a2fec483d8cb9c454
Vladimir Karashchuk <vovakaravov@gmail.com>
2024-01-17 19:11:57 -0500

    Quartz sync: Jan 17, 2024, 7:11 PM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -3,7 +3,7 @@ import * as Component from "./quartz/components"
 
 // components shared across all pages
 export const sharedPageComponents: SharedLayout = {
-  head: Component.Head(),
+  //head: Component.Head(),
   header: [],
   footer: Component.Footer({
     links: {
@@ -16,7 +16,7 @@ export const sharedPageComponents: SharedLayout = {
 // components for pages that display a single page (e.g. a single note)
 export const defaultContentPageLayout: PageLayout = {
   beforeBody: [
-    //Component.Breadcrumbs(),
+    Component.Breadcrumbs(),
     Component.ArticleTitle(),
     Component.ContentMeta(),
     Component.TagList(),
@@ -29,7 +29,7 @@ export const defaultContentPageLayout: PageLayout = {
     Component.DesktopOnly(Component.Explorer()),
   ],
   right: [
-    //Component.Graph(),
+    Component.Graph(),
     Component.DesktopOnly(Component.TableOfContents()),
     Component.Backlinks(),
   ],
